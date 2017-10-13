c2547afb03d0b4dba64d45f2b86d17d145340fa0
xiyanglove <627290749@qq.com>
2017-10-13 13:37:57 +0800

    提交

--- WeChatXy/urls.py
+++ WeChatXy/urls.py
@@ -17,7 +17,9 @@ from django.conf.urls import url
 from django.contrib import admin
 from RoBot import views
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^index/', views.index),
+    url(r'^wechat/', views.wechat),
+
 ]
